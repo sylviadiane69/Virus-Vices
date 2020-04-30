ccf38456b6f1adb031d6c50671085dd7f898d4b8
Sylvia Soto <sylviadiane69@gmail.com>
2020-04-29 20:51:01 -0500

    changes to controller file

--- controllers/vv_controller.js
+++ controllers/vv_controller.js
@@ -3,4 +3,37 @@ var router = express.Router();
 
 const virus = require('../models/virusVices');
 
-module.exports =  router;
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cards.shuffleAll(function(data) {
+    var hbsObject = {
+      cards: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/cards", function(req, res) {
+  cards.insertOne([
+    "black_card", "Question?"
+  ], [
+    req.body.name, req.body.sleepy
+  ], function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.post("/api/cards", function(req, res) {
+    cards.insertOne([
+      "white_card", "Answer!"
+    ], [
+      req.body.name, req.body.sleepy
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+
+module.exports = router;
