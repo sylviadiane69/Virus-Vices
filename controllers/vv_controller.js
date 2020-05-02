e7a4852213b038e6590e9e5c10e76c96b50ef4e5
Munioro <munioro27@gmail.com>
2020-05-02 10:36:26 -0500

    removed extra semicolon

--- controllers/vv_controller.js
+++ controllers/vv_controller.js
@@ -2,7 +2,7 @@ const express = require('express')
 var router = express.Router()
 
 // const virus = require('../models/virusVices')
-const orm = require('../config/orm');
+const orm = require('../config/orm')
 
 router.get('/', (req, res) => {
   res.render('home')
